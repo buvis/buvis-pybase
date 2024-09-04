23c773207ed7d47c161c001b337a878c285f73a3
Tomáš Bouška <tomas@buvis.net>
2024-09-04 20:43:13 +0200

    Group file metadata functions

--- src/buvis/pybase/filesystem/__init__.py
+++ src/buvis/pybase/filesystem/__init__.py
@@ -1,6 +1,3 @@
-from .file_creation_datetime import (
-    get_file_creation_datetime,
-    get_file_first_commit_datetime,
-)
+from .file_metadata.file_metadata_reader import FileMetadataReader
 
-__all__ = ["get_file_creation_datetime", "get_file_first_commit_datetime"]
+__all__ = ["FileMetadataReader"]
