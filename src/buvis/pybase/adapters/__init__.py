fe34736d582d756c9918f47cc229e10e1ebc0e42
Tomáš Bouška <tomas@buvis.net>
2025-12-23 00:07:23 +0100

    Add uv adapter

--- src/buvis/pybase/adapters/__init__.py
+++ src/buvis/pybase/adapters/__init__.py
@@ -7,11 +7,15 @@ from .console.console import console, logging_to_console
 from .jira.jira import JiraAdapter
 from .poetry.poetry import PoetryAdapter
 from .shell.shell import ShellAdapter
+from .uv.uv import UvAdapter
+from .uv.uv_tool import UvToolManager
 
 __all__ = [
     "JiraAdapter",
     "PoetryAdapter",
     "ShellAdapter",
+    "UvAdapter",
+    "UvToolManager",
     "console",
     "logging_to_console",
 ]
