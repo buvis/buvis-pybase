9e1a4940bbc6355dfba55dd87e08dc3fdba71e9f
Tomáš Bouška <tomas@buvis.net>
2026-01-05 07:19:30 +0100

    test(formatting): add StringOperator test file structure

---
+++ tests/formatting/test_string_operator.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from buvis.pybase.formatting.string_operator.string_operator import StringOperator  # noqa: F401
+
+
+class TestCollapse:
+    pass  # Placeholder
