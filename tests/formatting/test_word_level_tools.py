fb23b8a48b08406b5bf75d162d89202712fec526
Tomáš Bouška <tomas@buvis.net>
2026-01-05 07:27:19 +0100

    test(formatting): add WordLevelTools test file structure

---
+++ tests/formatting/test_word_level_tools.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from buvis.pybase.formatting.string_operator.word_level_tools import WordLevelTools  # noqa: F401
+
+
+class TestSingularize:
+    pass
+
+
+class TestPluralize:
+    pass
