3b0423caab0610857ed69522169d837c6c3f46d6
Tomáš Bouška <tomas@buvis.net>
2026-01-08 21:30:26 +0100

    feat(jira): export all public classes from jira adapter module

--- src/buvis/pybase/adapters/jira/__init__.py
+++ src/buvis/pybase/adapters/jira/__init__.py
@@ -1,4 +1,18 @@
+from .exceptions import (
+    JiraError,
+    JiraLinkError,
+    JiraNotFoundError,
+    JiraTransitionError,
+)
 from .jira import JiraAdapter
 from .settings import JiraFieldMappings, JiraSettings
 
-__all__ = ["JiraAdapter", "JiraFieldMappings", "JiraSettings"]
+__all__ = [
+    "JiraAdapter",
+    "JiraError",
+    "JiraFieldMappings",
+    "JiraLinkError",
+    "JiraNotFoundError",
+    "JiraSettings",
+    "JiraTransitionError",
+]
